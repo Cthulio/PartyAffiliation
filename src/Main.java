be1c79793351e464e5ec42dc3d5df0741f65e774
Nolan Crone <crone.nolan@gmail.com>
2025-02-05 21:08:32 -0500

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //class PartyFinder
+        //  main()
+        //      num misFortune = Random(4)
+        //      Input “Press Enter for a (mis)fortune.”
+        //      if misFortune == "0” then
+        //          Output ”I regret to inform you that you just lost The Game.”
+        //      else if misFortune == "1” then
+        //          Output "You are now manually breathing."
+        //      else if misFortune == "2” then
+        //          Output "This sentence is false."
+        //      else if misFortune == "3” then
+        //          Output "Your (un)lucky number is: " + misFortune + “.”
+        //      end if
+        //  return
+        //end class
+    }
+}
