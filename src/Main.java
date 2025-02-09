9b3f2b5629be5af0c17a6bd6daa6b1162f9bce3c
Nolan Crone <crone.nolan@gmail.com>
2025-02-08 19:09:49 -0500

    finalized code, ran checks.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,34 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //class PartyFinder
         //  main()
-        //      num misFortune = Random(4)
-        //      Input “Press Enter for a (mis)fortune.”
-        //      if misFortune == "0” then
-        //          Output ”I regret to inform you that you just lost The Game.”
-        //      else if misFortune == "1” then
-        //          Output "You are now manually breathing."
-        //      else if misFortune == "2” then
-        //          Output "This sentence is false."
-        //      else if misFortune == "3” then
-        //          Output "Your (un)lucky number is: " + misFortune + “.”
+        //      String yourParty
+        //      Output “Do you choose (D)emocrat, (I)ndependant, or (R)epublican?”
+        //      Input yourParty
+        //      if yourParty == "R” then
+        //          Output "You get a Republican Elephant."
+        //      else if yourParty == "D” then
+        //          Output "You get a Democratic Donkey."
+        //      else if yourParty == "I” then
+        //          Output "You get an Independant Man."
         //      end if
         //  return
         //end class
+        Scanner in = new Scanner(System.in);
+        String yourParty; //I want to do a char, but we haven't started indexes and the assignment calls for a String.
+        System.out.println("Do you choose (D)emocrat, (I)ndependant, or (R)epublican?");
+        yourParty = in.nextLine();//next().charAt(0) could also work here for char
+        if(yourParty.equalsIgnoreCase("R")) {//the prompt asks for the first letter I used the equalsIgnoreCase from the example video.
+            System.out.println("You get a Republican Elephant.");
+        } else if(yourParty.equalsIgnoreCase("D")) {
+            System.out.println("You get a Democratic Donkey.");
+        } else if(yourParty.equalsIgnoreCase("I")) {
+            System.out.println("You get an Independent Man.");
+        } else {
+            System.out.println("You get the Other Otter!");
+        }
     }
 }
